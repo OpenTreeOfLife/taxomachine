dbf89f0200fba825bb2a48476044bf0ac759a9cb
Cody Hinchliff <cody.hinchliff@gmail.com>
2012-11-13 16:36:53 -0500

    fixed download for forester.jar

--- mvn_install_forester.sh
+++ mvn_install_forester.sh
@@ -1,2 +1,2 @@
-wget -O ~/Downloads/forester_1005.jar http://code.google.com/p/forester/downloads/detail?name=forester_1005.jar&can=2&q=
+wget -O ~/Downloads/forester_1005.jar http://forester.googlecode.com/files/forester_1005.jar
 mvn install:install-file -Dfile=$HOME/Downloads/forester_1005.jar -DgroupId=org.forester -DartifactId=forester -Dversion=1.005 -Dpackaging=jar -DcreateChecksum=true
