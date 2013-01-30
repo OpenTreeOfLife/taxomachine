e10cf7b074bdfc84a177c50075fd929da350fea5
Cody Hinchliff <codiferous@gmail.com>
2013-01-30 14:26:42 -0700

    creating name dump feature (phylotastic compatible)

--- src/main/java/opentree/TaxonomySynthesizer.java
+++ src/main/java/opentree/TaxonomySynthesizer.java
@@ -67,7 +67,7 @@ public class TaxonomySynthesizer extends Taxonomy {
                 if (l.hasProperty("parentid"))
                     taxUId = String.valueOf(l.getProperty("parentid")); */
             
-            for (Relationship l : TAXCHILDOF_OUTGOING_TRAVERSAL.evaluator(Evaluators.toDepth(1)).traverse(n).relationships()) {
+            for (Relationship l : n.getRelationships(Direction.OUTGOING, RelType.TAXCHILDOF)) {
                 
                 System.out.println("start node: " + String.valueOf(l.getStartNode().getId()) + "; end node: " + String.valueOf(l.getEndNode().getId()));
 
