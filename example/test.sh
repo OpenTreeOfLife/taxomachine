7f62864ec69e4bdbc593191af0868b69d0ac6b0b
Stephen Smith <blackrim@gmail.com>
2013-02-28 15:11:24 -0500

    updated script

--- example/test.sh
+++ example/test.sh
@@ -1,3 +1,4 @@
 java -jar ../target/taxomachine-0.0.1-SNAPSHOT-jar-with-dependencies.jar inittax ncbi Dip.ncbi dip.db
 java -jar ../target/taxomachine-0.0.1-SNAPSHOT-jar-with-dependencies.jar addtax gbif Dip.gbif dip.db
 java -jar ../target/taxomachine-0.0.1-SNAPSHOT-jar-with-dependencies.jar graftbycomp dip.db gbif
+java -jar ../target/taxomachine-0.0.1-SNAPSHOT-jar-with-dependencies.jar makeottol dip.db
