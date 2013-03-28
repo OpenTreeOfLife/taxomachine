3dba52079740e359efc9e28dc2b42d202e606d2d
Cody Hinchliff <cody.hinchliff@gmail.com>
2013-03-28 12:50:27 -0400

    updates to tnrs results conversion

---
+++ src/main/java/opentree/tnrs/ContextResult.java
@@ -0,0 +1,14 @@
+package opentree.tnrs;
+
+import java.util.Collection;
+import java.util.LinkedList;
+
+public class ContextResult {
+	public String contextName;
+	public LinkedList<String> namesNotMatched;
+	
+	public ContextResult(String contextName, Collection<String> namesNotMatched) {
+		this.contextName = contextName;
+		this.namesNotMatched.addAll(namesNotMatched);
+	}
+}
