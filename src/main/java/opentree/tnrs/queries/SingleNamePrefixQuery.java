d751cb61c15d2bb27e188feb58398b09e168df4b
Cody Hinchliff <cody.hinchliff@gmail.com>
2013-08-23 12:08:44 -0400

    tweaking tnrs search

--- src/main/java/opentree/tnrs/queries/SingleNamePrefixQuery.java
+++ src/main/java/opentree/tnrs/queries/SingleNamePrefixQuery.java
@@ -68,7 +68,7 @@ public class SingleNamePrefixQuery extends AbstractBaseQuery {
     	this.queryString = queryString; // QueryParser.escape(queryString);
 
     	// build a phrase query
-    	String[] nameParts = queryString.split("\\s");
+    	String[] nameParts = queryString.split(" ");
     	for (String part : nameParts) {
     		phraseQueryForFullName.add(new Term("name", part));
     	}
