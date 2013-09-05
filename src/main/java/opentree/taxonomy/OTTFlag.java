beab7f541c0bee13c4465c516e721775040cde11
Cody Hinchliff <cody.hinchliff@gmail.com>
2013-09-05 14:51:49 -0400

    updating ott flags

--- src/main/java/opentree/taxonomy/OTTFlag.java
+++ src/main/java/opentree/taxonomy/OTTFlag.java
@@ -5,76 +5,77 @@ public enum OTTFlag {
 	// ==== flags designating suppression unless overriden
 	
 	/**
-	 * not a valid taxon. never made available for mapping
+	 * Not a valid taxon--never made available for mapping. This is applied to containers such as "Bacteria incertae sedis",
+	 * which in no way can ever represent biological lineages.
 	 */
 	NOT_OTU ("not_otu", false),
 	
 	// dubious flags: guilty until proven innocent
 	
 	/**
-	 * higher taxa with zero children...
+	 * Higher taxa with zero species-level children.
 	 */
 	BARREN ("barren", false),
 	
 	/**
-	 * things with the string "environmental" in the name
+	 * Things with the string "environmental" in their name.
 	 */
 	ENVIRONMENTAL ("environmental", false),
 	
 	/**
-	 * taxa of known hybrid origin
+	 * Taxa of known hybrid origin.
 	 */
 	HYBRID ("hybrid", false),
 
 	/**
-	 * low-rank children of high-rank taxa (e.g. genus child of class)
+	 * Low-rank children of high-rank taxa (e.g. genus child of class).
 	 */
 	MAJOR_RANK_CONFLICT ("major_rank_conflict", false),
 
 	/**
-	 * children of unclassified containers
+	 * Children of unclassified containers.
 	 */
 	UNCLASSIFIED_INHERITED ("unclassified_inherited", false),
 	
 	/**
-	 * ?
+	 * Taxa with the string "unclassified" in their name, but which have no children and thus are (hopefully) not a container.
 	 */
 	UNCLASSIFIED_DIRECT ("unclassified_direct", false),
 	
 	/**
-	 * viruses
+	 * Viruses.
 	 */
 	VIRAL ("viral", false),
 
 	// ===== flags not designating suppression
 	
 	/**
-	 * ?
+	 * Children of incertae sedis containers.
 	 */
 	INCERTAE_SEDIS ("incertae_sedis", true),
 	
 	/**
-	 * ?
+	 * Taxa with the string "incertae sedis" in their name, but which have no children and thus are (hopefully) not a container.
 	 */
 	INCERTAE_SEDIS_DIRECT ("incertae_sedis_direct", true),
 
 	/**
-	 * contained with a species-rank taxon
+	 * Taxa contained within a species-rank taxon.
 	 */
 	INFRASPECIFIC ("infraspecific", true),
 
 	/**
-	 * a sibling is of a lower (higher?) rank than this taxon
+	 * Taxa with a sibling which is of a lower (higher?) rank than the taxon.
 	 */
 	SIBLING_LOWER ("sibling_lower", true),
 	
 	/**
-	 * a sibling is of a higher (lower?) rank than this taxon
+	 * Taxa with a sibling which is of a higher (lower?) rank than the taxon.
 	 */
 	SIBLING_HIGHER ("sibling_higher", true),
 
 	/**
-	 * designates that the taxon should not be suppressed even if it has flags designating suppression
+	 * Designates that the taxon should not be suppressed even if it has flags designating suppression.
 	 */
 	FORCE_VISIBLE ("forced_visible", true);
 	
