8a5652d181c8cc875aa30708546ee3a3c5dc672c
Cody Hinchliff <cody.hinchliff@gmail.com>
2013-12-03 11:30:13 -0500

    adding dependency install script

---
+++ mvn_install_dependencies.sh
@@ -0,0 +1,11 @@
+# install jade repo
+cd ../
+git clone git@github.com:FePhyFoFum/jade.git
+cd jade
+sh mvn_install.sh
+
+# install ot-base repo
+cd ../
+git clone git@github.com:OpenTreeOfLife/ot-base.git
+cd ot-base
+sh mvn_install.sh
