2100a8ada4c4805b1f44468269422d8ae7111ec6
Cody Hinchliff <cody.hinchliff@gmail.com>
2014-09-21 10:25:31 -0400

    simplifying install script

--- setup_for_travis.sh
+++ setup_for_travis.sh
@@ -2,33 +2,23 @@
 TAXOMACHINE_HOME=pwd
 
 # download and set up ott
-OTT_SOURCENAME="aster"
-OTT_DOWNLOADDIR=$PREFIX"/data"
-mkdir $OTT_DOWNLOADDIR
-cd $OTT_DOWNLOADDIR
-wget "http://files.opentreeoflife.org/ott/$OTT_SOURCENAME.tgz"
-tar -xvf $OTT_SOURCENAME.tgz
-OTT_SOURCEDIR="$OTT_DOWNLOADDIR/$OTT_SOURCENAME"
-OTT_TAXONOMY=$OTT_SOURCEDIR"/taxonomy.tsv"
-OTT_SYNONYMS=$OTT_SOURCEDIR"/synonyms.tsv"
-OTT_DEPRECATED=$OTT_SOURCEDIR"/deprecated.tsv"
+wget "http://files.opentreeoflife.org/ott/aster.tgz"
+tar -xvf aster.tgz
+OTT_TAXONOMY="aster/taxonomy.tsv"
+OTT_SYNONYMS="aster/synonyms.tsv"
+OTT_DEPRECATED="aster/deprecated.tsv"
 
 # pull from the git repo and remove the binary if updating is turned on
-TAXOMACHINE_JAR="taxomachine-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
-TAXOMACHINE_COMPILE_LOCATION="$TAXOMACHINE_HOME/target/$TAXOMACHINE_JAR"
+TAXOMACHINE_JAR="target/taxomachine-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
 
 # compile taxomachine if necessary
 mvn_cmdline.sh
-TAXOMACHINE_COMMAND="java -jar $TAXOMACHINE_COMPILE_LOCATION "
-
-# prepare for dealing with the neo4j server
-TAXO_NEO4J_HOME="$PREFIX/neo4j-taxomachine"
-TAXO_NEO4J_DAEMON="$TAXO_NEO4J_HOME/bin/neo4j"
+TAXOMACHINE_COMMAND="java -jar $TAXOMACHINE_JAR "
 
 # download neo4j
 wget "http://neo4j.com/artifact.php?name=neo4j-community-1.9.8-unix.tar.gz"
 tar -xvf "artifact.php?name=neo4j-community-1.9.8-unix.tar.gz" # this name seems to be used on travis ci server
-mv neo4j-community-1.9.8 $TAXO_NEO4J_HOME
+mv neo4j-community-1.9.8 neo4j-server
 
 # build the ott db
 $TAXOMACHINE_COMMAND loadtaxsyn $OTT_SOURCENAME $OTT_TAXONOMY $OTT_SYNONYMS $TAXOMACHINE_DB	
@@ -38,22 +28,17 @@ $TAXOMACHINE_COMMAND adddeprecated $OTT_DEPRECATED $TAXOMACHINE_DB
     
 # point the server at the taxomachine db location
 TAXOMACHINE_DB_ASSIGNMENT="org.neo4j.server.database.location=$TAXOMACHINE_DB"
-SERVER_PROPERTIES="$TAXO_NEO4J_HOME/conf/neo4j-server.properties"
+SERVER_PROPERTIES="neo4j-server/conf/neo4j-server.properties"
 ORIG_SERVER_PROPERTIES="$SERVER_PROPERTIES.original"
-
-# edit the text
 mv "$SERVER_PROPERTIES" "$ORIG_SERVER_PROPERTIES"
-printf "$COMMENT\n$TAXOMACHINE_DB_ASSIGNMENT\n\n" > "$SERVER_PROPERTIES"
+printf "$TAXOMACHINE_DB_ASSIGNMENT\n\n" > "$SERVER_PROPERTIES"
 grep -v "org.neo4j.server.database.location" "$ORIG_SERVER_PROPERTIES" >> "$SERVER_PROPERTIES"
 
-# install the plugin if necessary
-PLUGIN="taxomachine-neo4j-plugins-0.0.1-SNAPSHOT.jar"
-PLUGIN_INSTALL_LOCATION="$TAXO_NEO4J_HOME/plugins/"$PLUGIN
-
-# compile the server plugins
+# compile and install the server plugin
 mvn_serverplugins.sh
-PLUGIN_COMPILE_LOCATION="$TAXOMACHINE_HOME/target/$PLUGIN"
+PLUGIN_COMPILE_LOCATION="target/taxomachine-neo4j-plugins-0.0.1-SNAPSHOT.jar"
+PLUGIN_INSTALL_LOCATION="neo4j-server/plugins/taxomachine-plugin.jar"
 mv "$PLUGIN_COMPILE_LOCATION" "$PLUGIN_INSTALL_LOCATION"
 
 # restart the server
-$TAXO_NEO4J_DAEMON restart
+neo4j-server/bin/neo4j restart
