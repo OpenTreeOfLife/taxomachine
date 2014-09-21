690e84f779b262465f28955c81afde1aa9f7d258
Cody Hinchliff <cody.hinchliff@gmail.com>
2014-09-21 09:07:13 -0400

    fixing install script

--- mvn_install_dependencies.sh
+++ mvn_install_dependencies.sh
@@ -1,11 +1,11 @@
 # install jade repo
 cd ../
-git clone git@github.com:FePhyFoFum/jade.git
+git clone http://github.com/FePhyFoFum/jade.git
 cd jade
 sh mvn_install.sh
 
 # install ot-base repo
 cd ../
-git clone git@github.com:OpenTreeOfLife/ot-base.git
+git clone http://github.com/OpenTreeOfLife/ot-base.git
 cd ot-base
 sh mvn_install.sh
