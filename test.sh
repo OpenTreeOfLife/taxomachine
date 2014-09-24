0e41bb3eaa6b17cdd6a0f60a5592398b74959f20
Cody Hinchliff <cody.hinchliff@gmail.com>
2014-09-24 14:52:25 -0400

    adding comments to clarify usage

--- test.sh
+++ test.sh
@@ -1,2 +1,8 @@
+# set the TAXOMACHINE_SERVER environment variable in your shell to point the script at the right location, e.g.:
+#
+# TAXOMACHINE_SERVER=devapi.opentreeoflife.org/taxomachine # to run remotely against devapi
+#
+# TAXOMACHINE_SERVER=localhost:7476/db/data # to run locally on devapi
+
 [ -z "$TAXOMACHINE_SERVER" ] && TAXOMACHINE_SERVER='localhost:7474/db/data' && export TAXOMACHINE_SERVER
 cd tests && nosetests -vs curl_tests.py
