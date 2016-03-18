2c82eca4cfd2b644a3eb502f1ead2a6c89b137c7
Jonathan A Rees <jar398@mumble.net>
2016-03-18 13:49:01 -0400

    more test fixes - localize to Asterales

--- ws-tests/test_v3_taxonomy_subtree_descendant_species.py
+++ ws-tests/test_v3_taxonomy_subtree_descendant_species.py
@@ -2,11 +2,25 @@
 
 from check import *
 
+# Argophyllaceae is a family in Asterales
+
 status = 0
 
 def check_result(result):
-    return (u'\)Argophyllaceae_ott3826;' in result and
-            u'\,Argophyllum_nitidum_ott5746190\,' in result)
+    newick = result[u'newick']
+    if not u')Argophyllaceae_ott3826;' in newick:
+        print '** Argophyllaceae_ott3826; not in newick'
+        print
+        print newick
+        print
+        return False
+    if not u',Argophyllum_nitidum_ott5746190,' in newick:
+        print '** Argophyllum_nitidum_ott5746190, not in newick'
+        print
+        print newick
+        print
+        return False
+    return True
 
 status += \
 simple_test('/v3/taxonomy/subtree',
--- ws-tests/test_v3_taxonomy_taxon_terminal_descendants.py
+++ ws-tests/test_v3_taxonomy_taxon_terminal_descendants.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 
+# Change to:
+#  Gerbera, Gerbera jamesonii, Gerbera cordata
+#  1021694, 1021698, 435564
+
+taxon = 1021694
+
 from check import *
 
 def check_result(result):
-    if not set([490099, 1066590]).issubset(set(result[u'terminal_descendants'])):
-        sys.stderr.write("Bos taurus (490099) and Bos primigenius (1066590) not returned as descendants of Bos (1066581)\n")
+    if not set([1021698, 435564]).issubset(set(result[u'terminal_descendants'])):
+        sys.stderr.write("hamesonii (1021698) and cordata (435564) not returned as descendants of Gerbera (1021694)\n")
         return False
     return True
 
@@ -12,7 +18,7 @@ status = 0
 
 status += \
 simple_test('/v3/taxonomy/taxon_info',
-            {u'ott_id': 1066581, u'list_terminal_descendants': True},
+            {u'ott_id': taxon, u'list_terminal_descendants': True},
             check_blob(extended_taxon_blob_fields +
                        [field(u'terminal_descendants', check_list(check_integer))]),
             check_result)
